1a70fc5883ae07c4fceb16c9684d665d547ef32a
Gileno <gilenoduarte.jobs@gmail.com>
2026-07-22 18:15:53 -0300

    fix: Add pading in header

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-accent text-primary w-full py-4 px-8 fixed top-0 z-10">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold text-primary">Portfólio</h1>
+        <h1 className="text-xl font-bold text-primary mr-3">Portfólio</h1>
         <nav>
           <ul className="flex space-x-4">
             <li><a href="#about" className="text-secondary hover:text-primary">Sobre</a></li>
