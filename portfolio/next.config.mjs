9f607f6c25f4136fc0852bd2ac29fa7a068e28ee
Gileno <gilenoduarte.jobs@gmail.com>
2026-07-24 16:56:13 -0300

    fix: Remove base path

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -2,7 +2,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    basePath: '/portifolio', // Remova se usar domínio próprio ou repositório raiz (usuario.github.io)
+    basePath: '/', // Remova se usar domínio próprio ou repositório raiz (usuario.github.io)
     assetPrefix: '/portifolio/'
   },
 };
