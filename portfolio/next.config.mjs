aca10f2de1f9b156ab0388c94c523e73ea47e451
Gileno <gilenoduarte.jobs@gmail.com>
2026-07-24 16:53:38 -0300

    fix: Add configs to deploy

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -2,6 +2,8 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    basePath: '/portifolio', // Remova se usar domínio próprio ou repositório raiz (usuario.github.io)
+    assetPrefix: '/portifolio/'
   },
 };
 
